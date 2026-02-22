fa6f1efbd4c6169a355e434956267582797159b4
abi <freellion@gmail.com>
2026-02-22 13:52:18 -0800

    add more pictures to gallery

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -8,9 +8,22 @@ import img4 from "../assets/IMG_4.jpeg";
 import img5 from "../assets/IMG_5.jpeg";
 import img6 from "../assets/IMG_6.jpeg";
 import img7 from "../assets/IMG_7.jpeg";
+import img8 from "../assets/IMG_8.jpeg";
+import img9 from "../assets/IMG_9.jpeg";
+import img10 from "../assets/IMG_10.jpeg";
+
+
+import img11 from "../assets/IMG_11.jpeg";
+import img12 from "../assets/IMG_12.jpeg";
+import img13 from "../assets/IMG_13.jpeg";
+import img14 from "../assets/IMG_14.jpeg";
+import img15 from "../assets/IMG_15.jpeg";
+import img16 from "../assets/IMG_16.jpeg";
+import img17 from "../assets/IMG_17.jpeg";
+
 
 export default function Gallery() {
-    const images = [img1, img2, img3, img4, img5, img6, img7];
+    const images = [img8, img9, img10, img11, img12, img13, img14, img15, img16, img1, img2, img3, img4, img5, img6, img7, img17];
     const [index, setIndex] = useState(0);
 
     const next = () => setIndex((i) => (i + 1) % images.length);
